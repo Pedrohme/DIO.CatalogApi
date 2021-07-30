3139cae91ff5fca7bee778862065a47bcdbf8d64
Pedrohme <pedrohmen45@gmail.com>
2021-07-30 16:43:21 -0300

    feat: added Song Entity

---
+++ DIO.CatalogApi/Entities/Song.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace DIO.CatalogApi.Entities {
+    public class Song {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Album { get; set; }
+        public string Author { get; set; }
+    }
+}
