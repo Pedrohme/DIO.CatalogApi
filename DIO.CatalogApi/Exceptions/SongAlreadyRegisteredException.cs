b23b1f5cd7c26141013a979eae460e03efa70c05
Pedrohme <pedrohmen45@gmail.com>
2021-07-30 16:31:32 -0300

    feat: added custom exceptions

---
+++ DIO.CatalogApi/Exceptions/SongAlreadyRegisteredException.cs
@@ -0,0 +1,6 @@
+﻿using System;
+namespace DIO.CatalogApi.Exceptions {
+    public class SongAlreadyRegisteredException : Exception {
+        public SongAlreadyRegisteredException() : base("This song has already been registered") { }
+    }
+}
---
+++ DIO.CatalogApi/Exceptions/SongNotRegisteredException.cs
@@ -0,0 +1,6 @@
+﻿using System;
+namespace DIO.CatalogApi.Exceptions {
+    public class SongNotRegisteredException : Exception {
+        public SongNotRegisteredException() : base("This song is not registered") { }
+    }
+}
