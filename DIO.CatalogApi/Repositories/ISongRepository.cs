6f0cbe120124b18dac1839fe61f5956aa91d254f
Pedrohme <pedrohmen45@gmail.com>
2021-07-30 16:43:48 -0300

    feat: added Song Repository Interface

---
+++ DIO.CatalogApi/Repositories/ISongRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DIO.CatalogApi.Entities;
+
+namespace DIO.CatalogApi.Repositories {
+    public interface ISongRepository : IDisposable {
+        Task<List<Song>> Get(int page, int numItems);
+
+        Task<Song> Get(Guid id);
+
+        Task<List<Song>> Get(string name, string author);
+
+        Task Insert(Song song);
+
+        Task Update(Song song);
+
+        Task Delete(Guid id);
+    }
+}
