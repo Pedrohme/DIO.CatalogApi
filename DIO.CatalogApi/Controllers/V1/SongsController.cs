7ad9fd788228112df359165908e5430bc6e25338
Pedrohme <pedrohmen45@gmail.com>
2021-07-30 04:43:05 -0300

    feat: added Songs Controller

---
+++ DIO.CatalogApi/Controllers/V1/SongsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DIO.CatalogApi.InputModel;
+using DIO.CatalogApi.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIO.CatalogApi.Controllers.V1 {
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase {
+
+        [HttpGet]
+        public async Task<ActionResult<List<SongViewModel>>> Get() {
+            return Ok();
+        }
+
+        [HttpGet("{songId:guid}")]
+        public async Task<ActionResult<SongViewModel>> Get(Guid songId) {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SongViewModel>> InsertSong(SongInputModel song) {
+            return Ok();
+        }
+
+        [HttpPut("{songId:guid}")]
+        public async Task<ActionResult> UpdateSong(Guid songId, SongInputModel song) {
+            return Ok();
+        }
+
+        [HttpPatch("{songId:guid}/album/{album:string}")]
+        public async Task<ActionResult> UpdateSong(Guid songId, string album) {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteSong(Guid songId) {
+            return Ok();
+        }
+    }
+}
