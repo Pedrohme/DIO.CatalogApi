4f74349fd49d53923acb88f7eff5b0261e06df88
Pedrohme <pedrohmen45@gmail.com>
2021-07-30 04:22:38 -0300

    feat: added Song ViewModel and InputModel

---
+++ DIO.CatalogApi/InputModel/SongInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DIO.CatalogApi.InputModel {
+    public class SongInputModel {
+        [Required]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "The song's name must contain at least one character")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "The album's name must contain at least one character")]
+        public string Album { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "The author's name must contain at least one character")]
+        public string Author { get; set; }
+    }
+}
---
+++ DIO.CatalogApi/ViewModel/SongViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace DIO.CatalogApi.ViewModel {
+    public class SongViewModel {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Album { get; set; }
+        public string Author { get; set; }
+    }
+}
