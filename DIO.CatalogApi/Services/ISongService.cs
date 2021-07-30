1a057a7ee46d91652a7a1415ef29d3778008ebda
Pedrohme <pedrohmen45@gmail.com>
2021-07-30 16:11:14 -0300

    feat: added Song service interface

---
+++ DIO.CatalogApi/Services/ISongService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DIO.CatalogApi.InputModel;
+using DIO.CatalogApi.ViewModel;
+
+namespace DIO.CatalogApi.Services {
+    public interface ISongService : IDisposable {
+
+        Task<List<SongViewModel>> Get(int page, int numItems);
+
+        Task<SongViewModel> Get(Guid id);
+
+        Task<SongViewModel> Insert(SongInputModel song);
+
+        Task Update(Guid id, SongInputModel song);
+
+        Task Update(Guid id, string album);
+
+        Task Delete(Guid id);
+    }
+}
